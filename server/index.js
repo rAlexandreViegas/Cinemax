3ac3ff84014f911d7708171b426a7590d3c5e638
Alexandre <alexandre_viegas@icloud.com>
2023-11-07 09:58:05 +0100

    🥅 Change console.log to res.send

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post("/api/save", (req, res) => {
   const imdbID = req.body.imdbID;
-  save(imdbID);
+  save(imdbID, res);
 });
 
 app.listen(PORT, () => console.log("Le serveur est lancé sur le port " + PORT));
--- server/modules/save.js
+++ server/modules/save.js
@@ -1,27 +1,37 @@
 const fs = require("fs");
 
-function save(movieID) {
-  fs.readFile("./data.json", "utf8", (err, data) => {
-    if (err) {
-      console.log("Une erreur s'est produite avec le serveur");
-    } else {
-      let jsonData = JSON.parse(data);
-
-      if (jsonData.favorites.includes(movieID)) {
-        console.log("Le film est déjà dans les favoris.");
+function save(movieID, response) {
+  try {
+    fs.readFile("./data.json", "utf8", (err, data) => {
+      if (err) {
+        response.send(
+          "Une erreur est survenue lors de l'ajout du film à vos favoris."
+        );
       } else {
-        jsonData.favorites.push(movieID);
+        let jsonData = JSON.parse(data);
 
-        fs.writeFile("./data.json", JSON.stringify(jsonData), (err) => {
-          if (err) {
-            console.log("Une erreur s'est produite avec le serveur");
-          } else {
-            console.log("Le film a bien été ajouté aux favoris");
-          }
-        });
+        if (jsonData.favorites.includes(movieID)) {
+          response.send("Le film est déjà dans les favoris.");
+        } else {
+          jsonData.favorites.push(movieID);
+
+          fs.writeFile("./data.json", JSON.stringify(jsonData), (err) => {
+            if (err) {
+              response.send(
+                "Une erreur est survenue lors de l'ajout du film à vos favoris."
+              );
+            } else {
+              res.send("Le film a bien été ajouté à vos favoris !");
+            }
+          });
+        }
       }
-    }
-  });
+    });
+  } catch (error) {
+    response.send(
+      "Une erreur est survenue lors de l'ajout du film à vos favoris."
+    );
+  }
 }
 
 module.exports = save;
