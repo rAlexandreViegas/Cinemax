7d8df3d2137626c6b71ee1a16c060deef5b4f601
Alexandre <alexandre_viegas@icloud.com>
2023-11-10 16:56:06 +0100

    👷 Create build v0.1

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const saveMovie = require("./modules/saveMovie");
 
@@ -8,11 +9,9 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("./client/build"));
 app.use(cors({ origin: "http://localhost:5000", credentials: true }));
-
-app.get("/", (req, res) => {
-  res.redirect("/");
-});
 
 app.post("/api/save", (req, res) => {
   const imdbID = req.body.imdbID;
@@ -21,6 +20,10 @@ app.post("/api/save", (req, res) => {
 
 app.get("/api/favorites", (req, res) => {
   res.sendFile(__dirname + "/data.json");
+});
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
 app.listen(PORT, () => console.log("Le serveur est lancé sur le port " + PORT));
