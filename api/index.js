0efef6a35b975cbcb357b9e6ff20953f1f46fb10
Alexandre <alexandre_viegas@icloud.com>
2023-11-13 22:02:49 +0100

    ♻️ Update saveMovie function

--- api/index.js
+++ api/index.js
@@ -1,16 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const path = require("path");
 
 const saveMovie = require("./modules/saveMovie");
 
 const app = express();
-
 const PORT = 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./client/build"));
 app.use(cors({ origin: "http://localhost:5000", credentials: true }));
 
 app.post("/api/save", (req, res) => {
@@ -19,11 +16,7 @@ app.post("/api/save", (req, res) => {
 });
 
 app.get("/api/favorites", (req, res) => {
-  res.sendFile(__dirname + "/data.json");
-});
-
-app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  res.sendFile(__dirname + "/tmp/data.json");
 });
 
 app.listen(PORT, () => console.log("Le serveur est lancé sur le port " + PORT));
--- api/modules/saveMovie.js
+++ api/modules/saveMovie.js
@@ -1,40 +1,29 @@
-const fs = require("fs");
+const fs = require("fs").promises;
+const path = require("path");
 
-function saveMovie(movieID, response) {
+async function saveMovie(movieID, response) {
   const newFavorite = { id: movieID[0], movie: movieID[1] };
-  let jsonData;
 
-  fs.readFile("./data.json", "utf8", (err, data) => {
-    if (err) {
-      response.send(err);
-      return;
+  try {
+    const filePath = path.join(__dirname, "../tmp/data.json");
+    const data = await fs.readFile(filePath, "utf8");
+    const jsonData = JSON.parse(data);
+
+    const movieExist = jsonData.favorites.some(
+      (favorite) => favorite.id === newFavorite.id
+    );
+
+    if (movieExist) {
+      response.status(409).send("Movie already in favorites.");
+    } else {
+      jsonData.favorites.push(newFavorite);
+
+      await fs.writeFile(filePath, JSON.stringify(jsonData));
+      response.status(201).send("Film added to favorites!");
     }
-
-    try {
-      jsonData = JSON.parse(data);
-
-      const movieExist = jsonData.favorites.some(
-        (favorite) => favorite.id === newFavorite.id
-      );
-
-      if (movieExist) {
-        response.status(409);
-      } else {
-        jsonData.favorites.push(newFavorite);
-
-        fs.writeFile("./data.json", JSON.stringify(jsonData), (err) => {
-          if (err) {
-            response.send(err);
-            return;
-          }
-
-          response.redirect("/favorites");
-        });
-      }
-    } catch (error) {
-      response.send(error);
-    }
-  });
+  } catch (error) {
+    response.status(500).send(error.message);
+  }
 }
 
 module.exports = saveMovie;
